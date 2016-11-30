2f0480f38f6c8d2ec3c33b1a7ea3c9413b636798
plexon21 <plexon21@bluewin.ch>
2016-11-30 16:54:35 +0100

    solved all 17-sudokus

--- src/ch/fhnw/ip5/sudoku/SolveAll.java
+++ src/ch/fhnw/ip5/sudoku/SolveAll.java
@@ -1,12 +1,16 @@
 package ch.fhnw.ip5.sudoku;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import ch.fhnw.ip5.sudoku.gui.SudokuGUI;
 import ch.fhnw.ip5.sudoku.reader.SudokuReader;
+import ch.fhnw.ip5.sudoku.solver.Backtrack;
 import ch.fhnw.ip5.sudoku.solver.methods.HiddenSingleMethod;
+import ch.fhnw.ip5.sudoku.solver.methods.HiddenSubSetMethod;
 import ch.fhnw.ip5.sudoku.solver.methods.NakedSingleMethod;
 import ch.fhnw.ip5.sudoku.solver.methods.NakedSubSetMethod;
 import ch.fhnw.ip5.sudoku.solver.methods.SolveMethod;
@@ -16,71 +20,84 @@ public class SolveAll {
 
 	public static void main(String[] args) {
 
-//		System.out.println("Sudoku has been solved. Believe me!");
-		
-		//"C:\\Programming\\IP-5_sudoku\\res\\parsed"
-		//"C:\\Users\\Simon\\OneDrive\\IP5-Sudoku\\Raetsel AG Sudoku\\06010054800_Archive_veryeasy_parsed"
-		//String sourceFolder = "C:\\Programming\\IP-5_sudoku\\res\\parsed";
-		String sourceFolder ="C:\\Programming\\IP-5_sudoku\\res\\parsed";
-		
+		// System.out.println("Sudoku has been solved. Believe me!");
+
+		// "C:\\Programming\\IP-5_sudoku\\res\\parsed"
+		// "C:\\Users\\Simon\\OneDrive\\IP5-Sudoku\\Raetsel AG
+		// Sudoku\\06010054800_Archive_veryeasy_parsed"
+		// String sourceFolder = "C:\\Programming\\IP-5_sudoku\\res\\parsed";
+		String sourceFolder = "C:\\Programming\\IP-5_sudoku\\res\\parsed";
+
 		ArrayList<Board> list = new ArrayList<>();
-		ArrayList<Board> solveableSudokus = new ArrayList<>();
-		
+		ArrayList<Board> solvedSudokus = new ArrayList<>();
+
 		File node = new File(sourceFolder);
 		String[] subNodes = node.list();
-		
+
 		try {
-			
+
 			for (String fileName : subNodes) {
 				list.addAll(SudokuReader.readFromFilename(sourceFolder + "\\" + fileName));
 			}
-			
+
 			SolveMethod m1 = new NakedSingleMethod();
 			SolveMethod m2 = new HiddenSingleMethod();
 			SolveMethod m3 = new NakedSubSetMethod();
-			
+			SolveMethod m4 = new HiddenSubSetMethod();
+
 			int numberOfSolvableWithGivenMethods = 0;
+			int numberOfBacktrackedSudokus = 0;
 			int numberOfSudokus = list.size();
-						
+
 			for (Board b : list) {
 				b.setupBoard();
 
-				Board givenBoard = new Board(b);
-				
 				boolean solving = true;
 				int m1counter = 0;
 				int m2counter = 0;
 				int m3counter = 0;
-				
-				while(solving) {
+				int m4counter = 0;
+				boolean wasBacktracked = false;
+
+				while (solving) {
 					if (m1.solve(b)) {
 						m1counter++;
 					} else if (m2.solve(b)) {
 						m2counter++;
 					} else if (m3.solve(b)) {
 						m3counter++;
+					} else if (m4.solve(b)) {
+						m4counter++;
 					} else {
 						solving = false;
 					}
-				}				
-				
-				if (b.isFilled()) {
+				}
+
+				if (b.isSolvedCorrectly()) {
 					numberOfSolvableWithGivenMethods++;
-					solveableSudokus.add(new Board(givenBoard));
+					solvedSudokus.add(new Board(b));
+				} else {
+					Backtrack.solve(b);
+					solvedSudokus.add(new Board(b));
+					numberOfSolvableWithGivenMethods++;
+					numberOfBacktrackedSudokus++;
+					wasBacktracked = true;
 				}
 
 			}
-			
-			System.out.println("Total number of Sudoku = " + numberOfSudokus);
-			System.out.println("Number of solvable Sudokus with given Methods = " + numberOfSolvableWithGivenMethods);
-			
-			
-			
+			String target = sourceFolder + "\\" + "17er_solved";
+			try (FileWriter writer = new FileWriter(target)) {
+				for (Board b : solvedSudokus) {
+					String sudokuString = b.createBoardString();
+					writer.write(sudokuString + "\n");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
 
 	}
 
